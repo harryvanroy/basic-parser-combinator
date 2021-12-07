5fe6956e34775172c1f6edccc78789b81871bb47
harryvanroy <harrison.vanroy007@gmail.com>
2021-12-07 22:09:56 +1000

    json parser

--- app/Main.hs
+++ app/Main.hs
@@ -2,18 +2,22 @@
 
 module Main where
 
-import Control.Applicative (Alternative (empty, (<|>)))
-import Data.Char (isAlpha, isAlphaNum, isDigit, isLower, isUpper, toUpper)
+import Control.Applicative (Alternative (empty, many, some, (<|>)))
+import Data.Char (isAlpha, isAlphaNum, isDigit, isLower, isSpace, isUpper, toUpper)
+import System.Environment (getArgs)
 
 type Input = String
 
 newtype Parser a = Parser {runParser :: Input -> Maybe (a, Input)}
 
--- >>> runParser (fmap toUpper item) "abc"
--- Just ('A',"bc")
-
--- >>> runParser (fmap toUpper item) ""
--- Nothing
+data JsonValue
+  = JsonNull
+  | JsonBool Bool
+  | JsonString String
+  | JsonNumber Int
+  | JsonArray [JsonValue]
+  | JsonObject [(String, JsonValue)]
+  deriving (Show, Eq)
 
 instance Functor Parser where
   -- fmap :: (a->b) -> Parser a -> Parser b
@@ -23,9 +27,6 @@ instance Functor Parser where
           Nothing -> Nothing
           Just (a, rest) -> Just (f a, rest)
       )
-
---- >>> runParser (pure toUpper <*> item ) "abc"
--- Just ('A',"bc")
 
 instance Applicative Parser where
   -- pure :: a -> Parser a
@@ -60,43 +61,128 @@ instance Alternative Parser where
           Just (a, rest) -> Just (a, rest)
       )
 
-item :: Parser Char
-item =
+itemP :: Parser Char
+itemP =
   Parser
     ( \case
         [] -> Nothing
         (c : cs) -> Just (c, cs)
     )
 
-sat :: (Char -> Bool) -> Parser Char
-sat p = do
-  x <- item
+satP :: (Char -> Bool) -> Parser Char
+satP p = do
+  x <- itemP
   if p x then return x else empty
 
-digit :: Parser Char
-digit = sat isDigit
+digitP :: Parser Char
+digitP = satP isDigit
 
-lower :: Parser Char
-lower = sat isLower
+charP :: Char -> Parser Char
+charP x = satP (== x)
 
-upper :: Parser Char
-upper = sat isUpper
-
-letter :: Parser Char
-letter = sat isAlpha
-
-alphaNum :: Parser Char
-alphaNum = sat isAlphaNum
-
-char :: Char -> Parser Char
-char x = sat (== x)
-
-string :: String -> Parser String
-string [] = return []
-string (x : xs) = do
-  char x
-  string xs
+stringP :: String -> Parser String
+stringP [] = empty
+stringP (x : xs) = do
+  charP x
+  stringP xs
   return (x : xs)
 
+natP :: Parser Int
+natP =
+  do
+    xs <- some digitP
+    return (read xs)
+
+intP :: Parser Int
+intP =
+  do
+    charP '-'
+    n <- natP
+    return (- n)
+    <|> natP
+
+spaceP :: Parser ()
+spaceP = do
+  many (satP isSpace)
+  return ()
+
+withSpace :: Parser a -> Parser a
+withSpace p = do
+  spaceP
+  v <- p
+  spaceP
+  return v
+
+sepBy' :: Parser a -> Parser b -> Parser [b]
+sepBy' sep p = do
+  x <- p
+  xs <- many (sep >> p)
+  return (x : xs)
+
+jsonNullP :: Parser JsonValue
+jsonNullP = JsonNull <$ stringP "null"
+
+jsonBoolP :: Parser JsonValue
+jsonBoolP = trueP <|> falseP
+  where
+    trueP = JsonBool True <$ stringP "true"
+    falseP = JsonBool False <$ stringP "false"
+
+jsonNumberP :: Parser JsonValue
+jsonNumberP = JsonNumber <$> intP
+
+stringQuotesP :: Parser String
+stringQuotesP = do
+  charP '"'
+  xs <- many (satP (/= '"'))
+  charP '"'
+  return xs
+
+jsonStringP :: Parser JsonValue
+jsonStringP = JsonString <$> stringQuotesP
+
+jsonArrayP :: Parser JsonValue
+jsonArrayP = do
+  charP '['
+  xs <- withSpace $ sepBy' (withSpace $ charP ',') jsonValueP <|> pure []
+  charP ']'
+  return $ JsonArray xs
+
+jsonObjectP :: Parser JsonValue
+jsonObjectP = do
+  charP '{'
+  kvs <- withSpace $ sepBy' (withSpace $ charP ',') jsonKeyValueP <|> pure []
+  charP '}'
+  return $ JsonObject kvs
+
+jsonKeyValueP :: Parser (String, JsonValue)
+jsonKeyValueP = do
+  k <- stringQuotesP
+  withSpace $ charP ':'
+  v <- jsonValueP
+  return (k, v)
+
+jsonValueP :: Parser JsonValue
+jsonValueP =
+  jsonNullP
+    <|> jsonBoolP
+    <|> jsonStringP
+    <|> jsonNumberP
+    <|> jsonArrayP
+    <|> jsonObjectP
+
+jsonP :: Parser JsonValue
+jsonP = withSpace $ jsonObjectP <|> jsonArrayP
+
+parseJsonFile :: FilePath -> IO JsonValue
+parseJsonFile filePath = do
+  x <- readFile filePath
+  case runParser jsonP x of
+    Nothing -> error "parse error"
+    Just (v, _) -> return v
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  args <- getArgs
+  result <- parseJsonFile $ head args
+  print result
